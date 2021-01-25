9a25ddc5d8d44334c066ab62b59aa3d187d5c722
Luke Tebbs <luke.tebbs@digital.hmrc.gov.uk>
2021-01-25 11:36:29 +0000

    add in initial outline of functions.

--- frontend/src/main/scala/Frontend.scala
+++ frontend/src/main/scala/Frontend.scala
@@ -120,4 +120,50 @@ class ClassOne(json: String) extends js.Object {
     )
     l.toString + "," + h.toString
   }
+
+  object interestOnLateClassOne {
+
+    private def sampleResponse(rowsIn: List[InterestRow]) = new js.Object {
+      val totalDebt: Double = 1
+      val totalInterest: Double = 2
+      val grandTotal: Double = 3
+
+      val rows: js.Array[js.Object] = rowsIn.zipWithIndex.map { case (row,i) => 
+        new js.Object {
+          val periodStart = row.periodStart
+          val debt = row.debt
+          val interestDue = (i+1) * 100
+        } : js.Object
+      }.toJSArray
+    }
+
+    def calculate(
+      rows: js.Array[InterestRow],
+      remissionPeriod: RemissionPeriod
+    ): js.Object = sampleResponse(rows.toList)
+
+    def calculate(
+      rows: js.Array[InterestRow]
+    ): js.Object = sampleResponse(rows.toList)
+
+
+    def getRates(): js.Array[js.Object] = {2010 to 2020}.map { yearGen => 
+      new js.Object {
+        val year = yearGen
+        val rate = 0.055
+      }: js.Object
+    }.toJSArray
+  }
 }
+
+@JSExportTopLevel("InterestRow")
+class InterestRow(
+  val periodStart: js.Date,
+  val debt: Double
+)
+
+@JSExportTopLevel("RemissionPeriod")
+class RemissionPeriod(
+  val start: js.Date,
+  val end: js.Date
+)
